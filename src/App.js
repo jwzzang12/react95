f670617edb3df7e7b27fd8cd89ed028fb369c658
jwzzang12 <moonbb12@gmail.com>
2023-10-06 20:49:07 +0100

    Trigger Alert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { Button, ThemeProvider, Alert } from '@react95/core';
 import '@react95/icons/icons.css';
-import { Button, ThemeProvider } from '@react95/core';
+import React from 'react';
 
 function App() {
+  const [showAlert, toggleShowAlert] = React.useState(false);
+
+  const handleOpenAlert = () => toggleShowAlert(true);
+  const handleCloseAlert = () => toggleShowAlert(false);
+
   return (
     <ThemeProvider>
-      <Button>Click me!</Button>
+      <div
+        style={{
+          marginBottom: 10,
+        }}
+      ></div>
+      <Button onClick={handleOpenAlert}>Trigger Alert</Button>
+      {showAlert && (
+        <Alert
+          title='Windows Networking'
+          type='info'
+          message='The Windows password you typed is incorrect.'
+          closeAlert={handleCloseAlert}
+          buttons={[
+            {
+              value: 'OK',
+              onClick: handleCloseAlert,
+            },
+          ]}
+        />
+      )}
     </ThemeProvider>
   );
 }
